13eeb94dcc2c861b94b8722ce4604f5c4a0c47d6
Shoaib <shoaib.omar.17@ucl.ac.uk>
2017-10-28 23:32:26 +0100

    mehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- places.php
+++ places.php
@@ -11,13 +11,14 @@ function GetData(){
         CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
         CURLOPT_TIMEOUT        => 120,    // time-out on response
     ); 
-	$curl = curl_init("https://maps.googleapis.com/maps/api/place/textsearch/xml?query=tourist+attractions+in+".$_GET['city']."&key=");//AIzaSyCt5eOwxpf76CDhdCoBUQ3ICzi-QwB4-eo
+	$curl = curl_init("https://maps.googleapis.com/maps/api/place/textsearch/xml?query=tourist+attractions+in+".$_GET['city']."&key=AIzaSyCt5eOwxpf76CDhdCoBUQ3ICzi-QwB4-eo");//AIzaSyCt5eOwxpf76CDhdCoBUQ3ICzi-QwB4-eo
 	curl_setopt_array($curl, $options);
 	$xml=curl_exec($curl);
 	echo $xml;
 	file_put_contents($_GET['city'].".xml", $xml);
 }
 
+unlink($_GET['city'].".xml");
 if (file_exists($_GET['city'].".xml")){
 	if ((time() - filemtime($_GET['city'].".xml")) > 86400){
 		unlink($_GET['city'].".xml");
