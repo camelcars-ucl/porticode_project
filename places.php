abbb4c86b4786e7fce7bf956a848294d1b87a671
Shoaib <shoaib.omar.17@ucl.ac.uk>
2017-10-28 17:57:11 +0100

    Ghetto hack to use google api pt 4

--- places.php
+++ places.php
@@ -1,3 +1,6 @@
 <? php
-echo http_get("https://maps.googleapis.com/maps/api/place/textsearch/xlm?query=tourist attractions in ".$_GET['city']."&key=AIzaSyB39gn3Le7ynVvxC-7GVvrtF3lIur60vsk", array("timeout"=>2), $info);
+echo "HI";
+$curl = curl_init("https://maps.googleapis.com/maps/api/place/textsearch/xlm?query=tourist attractions in ".$_GET['city']."&key=AIzaSyB39gn3Le7ynVvxC-7GVvrtF3lIur60vsk");
+
+echo curl_exec($curl);///http_get("https://maps.googleapis.com/maps/api/place/textsearch/xlm?query=tourist attractions in ".$_GET['city']."&key=AIzaSyB39gn3Le7ynVvxC-7GVvrtF3lIur60vsk", array("timeout"=>2), $info);
 ?>
