07c2cae9bd02655dd1462f5971479c7c695d2ce6
Shoaib <shoaib.omar.17@ucl.ac.uk>
2017-10-28 20:34:42 +0100

    Ghetto hack to use google api pt 22

--- places.php
+++ places.php
@@ -7,7 +7,7 @@ if (file_exists($_GET['city'].".xml")){
 	echo "HERE";
 	$curl = curl_init("https://maps.googleapis.com/maps/api/place/textsearch/xml?query=tourist+attractions+in+".$_GET['city']."&key=AIzaSyB39gn3Le7ynVvxC-7GVvrtF3lIur60vsk");
 	$xml=curl_exec($curl);
-	echo $xml;
+	echo gettype($xml);
 	echo json_encode($xml);
 	file_put_contents($_GET['city'].".xml", json_encode($xml));
 }
